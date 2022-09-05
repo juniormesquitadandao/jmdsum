d17e25f3ba557c385b3b021874abcde0cb8ccb15
juniormesquitadandao <juniormesquitadandao@gmail.com>
2022-09-05 16:49:35 +0000

    boards

--- app/boards.rb
+++ app/boards.rb
@@ -1,10 +1,10 @@
 def boards size
-  max = size ** 2
-  list1 = [' - '] * max
-  position = 0
+  list1 = [' - '] * (size ** 2)
+  x,y = [0, 0]
 
   loop do
     list2 = list1.dup
+    position = (x * size) + y
     list2[position] = ' @"'
 
     size.times do |i| 
@@ -15,10 +15,10 @@ def boards size
     puts `clear`
     puts list2.join
 
-    puts "Type new position, or less than 0, or greater than #{max - 1} to exit:"
-    position = gets.to_i
+    puts "Type new coordenates NxN, or less than 0x0, or greater than #{size - 1}x#{size - 1} to exit:"
+    x,y = gets.split('x').map(&:to_i)
 
-    break if !(0...max).include? position 
+    break if !(0...size).include?(x) || !(0...size).include?(y)
   end
 end
 
